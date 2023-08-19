8f94e265b40bca0c977cf4f5b0343b9491765470
Dimitar Tumbalov <mitko.tumbalov@abv.bg>
2023-08-20 01:54:13 +0300

    added create btn to side menu

---
+++ components/CreateEventModal/CreateEventModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CreateEventModal.module.scss';
+
+const CreateEventModal = () => {
+  return (
+    <div className={styles.}>
+
+    </div>
+  )
+}
+
+export default CreateEventModal
--- components/SideMenu/SideMenu.js
+++ components/SideMenu/SideMenu.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './SideMenu.module.scss';
+import CreateIcon from '../CreateIcon/CreateIcon';
 
 const SideMenu = () => {
   return (
     <div className={styles.container}>
-      Side Menu
+      <button className={styles.createBtn}>
+        <CreateIcon height='2.25rem' className={styles.createBtnIcon}/>
+        Create
+      </button>
     </div>
   )
 }
